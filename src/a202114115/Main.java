de764e6fc9e2e7698ae260ad732c71e662d8c196
Yang, soeun <su47992671@gmail.com>
2023-09-20 11:19:49 +0900

    two

--- src/a202114115/Main.java
+++ src/a202114115/Main.java
@@ -2,7 +2,8 @@ package a202114115;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("one 202114115"); 
+		Two.print();
+		 System.out.println("one 202114115");
 		}
 	
 }
